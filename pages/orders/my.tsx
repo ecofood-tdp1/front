bd111b4d0ca5c49e8e9acef57b755fcef24bc56b
Agustin Gabrielli <agabrielli@fi.uba.ar>
2023-06-04 13:03:23 -0300

    Order status added

--- pages/orders/my.tsx
+++ pages/orders/my.tsx
@@ -15,6 +15,7 @@ import axios from 'axios';
 import MyOrdersListActions from '../../components/MyOrdersListActions';
 import { GetOrdersOfUser } from '../../repository/OrderRepository';
 import { GetShop } from '../../repository/ShopRepository';
+import OrderStatusBadge from '../../components/OrderStatusBadge';
 
 const MyOrdersList = () => {
     const [orders, setOrders] = useState([]);
@@ -26,14 +27,14 @@ const MyOrdersList = () => {
     const fetchMyOrders = async () => {
         try {
             const orders = await GetOrdersOfUser("4016cb54-ff0e-46a6-ace5-69304d9720c7"); // TODO: user hardcoded
-    
+
             const ordersWithShops = await Promise.all(
                 orders.map(async (vanilla_order) => {
                     const shop_from_order = await GetShop(vanilla_order.shop_id);
                     return { order: vanilla_order, shop: shop_from_order };
                 })
             );
-    
+
             setOrders(ordersWithShops);
         } catch (error) {
             console.error('Error fetching orders:', error);
@@ -41,7 +42,7 @@ const MyOrdersList = () => {
     };
 
     return (
-        <TableContainer>
+        <TableContainer mt={5}>
             <Table variant='simple' colorScheme={"blackAlpha"}>
                 <TableCaption>Mis pedidos</TableCaption>
                 <Thead>
@@ -50,7 +51,7 @@ const MyOrdersList = () => {
                         <Th>Fecha</Th>
                         <Th>Horario de retiro</Th>
                         <Th>Ubicación</Th>
-                        <Th>Status</Th>
+                        <Th>Estado</Th>
                         <Th isNumeric>Acciones</Th>
                     </Tr>
                 </Thead>
@@ -68,7 +69,9 @@ const MyOrdersList = () => {
                                     <Td>{new Date(order.order.created_at).toLocaleDateString()}</Td>
                                     <Td>{order.shop.pick_up_from} a {order.shop.pick_up_to} hrs</Td>
                                     <Td>{order.shop.address} - {order.shop.neighborhood}</Td>
-                                    <Td>{order.order.status}</Td>
+                                    <Td>
+                                        <OrderStatusBadge orderStatus={order.order.status} />
+                                    </Td>
                                     <Td isNumeric>
                                         <MyOrdersListActions orderid={order.order._id} />
                                     </Td>
