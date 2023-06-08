a3e4a8fe6684e847f1e097cef133e90ef09d1f1b
melmombru <mmombru@fi.uba.ar>
2023-06-07 22:08:27 -0300

    Added shops profits and fix  neighborhood filter

---
+++ model/Wallet.tsx
@@ -0,0 +1,16 @@
+type Wallet = {
+    readonly balance: Balance,
+    readonly transactions: Transactions[],
+    readonly _id: string
+}
+
+type Balance = {
+    readonly amount: number,
+    readonly currency: string
+}
+
+type Transactions = {
+    readonly amount: number,
+    readonly currency: string,
+    readonly operation: string
+}
