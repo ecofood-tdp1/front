a79f6aee7d6cbcb415f82642f04a701b0c8402a5
Tomas Macia <tomasmacia@gmail.com>
2023-06-08 15:55:20 -0300

    better reviews

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,14 +1,14 @@
 import seedrandom from 'seedrandom';
 
 export const getReviewStarsFor = (id: string) => {
-    const seed = id;
+    const seed = id + "grupo4tdp";
     const rng = new seedrandom(seed);
     const randomNum = rng() * (5.3 - 3.4) + 3.4;
     return Math.round(randomNum)
 }
 
 export const getReviewCountFor = (id: string) => {
-    const seed = id;
+    const seed = id + "grupo4tdp";
     const rng = new seedrandom(seed);
     const randomNum = rng() * (20 - 2) + 2;
     return Math.round(randomNum)
