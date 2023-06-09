9b35d67e1d142c9215a514e7cd19fe766636a079
Agustin Gabrielli <agabrielli@fi.uba.ar>
2023-06-09 18:12:14 -0300

    Fix Ecofood logo navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -43,9 +43,9 @@ export default function NavBar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
+                  <Image
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src={logoEcofood}
                     alt="Your Company"
                   />
                   <Image
