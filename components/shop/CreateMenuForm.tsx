01afcbe7bfd62ba0ae4b4c78ccb48be8ea29aeed
manudelca <manueldelcarril@gmail.com>
2023-07-16 23:59:43 -0300

    Fixed scrolling

--- components/shop/CreateMenuForm.tsx
+++ components/shop/CreateMenuForm.tsx
@@ -21,6 +21,7 @@ import {
     Center,
     Spacer,
     Textarea,
+    SimpleGrid,
   } from '@chakra-ui/react';
   import { useState } from 'react';
 import { HiEye, HiEyeOff } from 'react-icons/hi'
@@ -75,74 +76,86 @@ import { HiEye, HiEyeOff } from 'react-icons/hi'
           align={{ lg: 'flex-start' }}
           spacing={{ base: '8', md: '16' }}
         >
-          <Stack spacing={{ base: '8', md: '10' }} flex="2">
-            <Flex alignItems="center" justifyContent="center" mb={4}>
-              <Heading as="h1" fontSize="3xl" fontWeight="bold" color="green.600">
-                Agregar pack
-              </Heading>
-            </Flex>
-    
-            <Stack spacing="12">
-              <FormControl id="name" isRequired>
-                  <FormLabel>Nombre del pack</FormLabel>
-                  <Input type="text" 
-                      placeholder="Elegí un nombre atractivo..."
-                      onInput={(e) => inputName(e)}
-                      _placeholder={{ color: 'gray.500' }}
-                  />
-              </FormControl>
-              <FormControl id="description" isRequired>
-                  <FormLabel>Descripción</FormLabel>
-                  <Textarea
-                      size='md'
-                      placeholder="Redactá una descripción atrapante..."
-                      onInput={(e) => inputDescription(e)}   
-                      _placeholder={{ color: 'gray.500' }}
-                  />
-              </FormControl>
-              <RadioGroup  onChange={setType} value={type}>
-              <FormControl id="type">
-                  <FormLabel>Seleccioná el tipo de pack</FormLabel>
-              </FormControl>  
-              <Flex>
-                <Spacer />
-                <Radio value='specific' spacing="4">
-                  <Heading fontSize="md" fontWeight="semibold">                        
-                    <Badge fontSize="md" colorScheme='green'>Específico</Badge>
+          <Stack spacing={{ base: '8', md: '10' }} flex="2">  
+            <SimpleGrid spacing="12" mt={4} mb={8} ml={4} mr={4} columns={1}>
+              <Box>
+                <Flex alignItems="center" justifyContent="center" mb={4}>
+                  <Heading as="h1" fontSize="3xl" fontWeight="bold" color="green.600">
+                    Agregar pack
                   </Heading>
-                  <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
-                    El pack debe contener todos los productos
-                  </Text>
-                </Radio>
-                <Spacer />
-                <Radio value='surprise' spacing="4">
-                  <Heading fontSize="md" fontWeight="semibold">                        
-                    <Badge fontSize="md" colorScheme='purple'>Sorpresa</Badge>
-                  </Heading>
-                  <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
-                    El pack puede contener algunos de los productos
-                  </Text>
-                </Radio>
-                <Spacer />
-              </Flex>
-              </RadioGroup>
-              <FormControl id="name" isRequired>
-                  <FormLabel>Stock disponible</FormLabel>
-                  <Input type="number" 
-                      placeholder="0"
-                      onInput={(e) => inputStock(e)}
-                      _placeholder={{ color: 'gray.500' }}
-                  />
-              </FormControl>
-              <FormControl id="name" isRequired>
-                  <FormLabel>Consumir antes de</FormLabel>
-                  <Input type="datetime-local"
-                      placeholder="0"
-                      onInput={(e) => inputDateTime(e)}
-                      _placeholder={{ color: 'gray.500' }}
-                  />
-              </FormControl>
-            </Stack>
+                </Flex>
+              </Box>
+              <Box>
+                <FormControl id="name" isRequired>
+                    <FormLabel>Nombre del pack</FormLabel>
+                    <Input type="text" 
+                        placeholder="Elegí un nombre atractivo..."
+                        onInput={(e) => inputName(e)}
+                        _placeholder={{ color: 'gray.500' }}
+                    />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="description" isRequired>
+                    <FormLabel>Descripción</FormLabel>
+                    <Textarea
+                        size='md'
+                        placeholder="Redactá una descripción atrapante..."
+                        onInput={(e) => inputDescription(e)}   
+                        _placeholder={{ color: 'gray.500' }}
+                    />
+                </FormControl>
+              </Box>
+              <Box>
+                <RadioGroup  onChange={setType} value={type}>
+                <FormControl id="type">
+                    <FormLabel>Seleccioná el tipo de pack</FormLabel>
+                </FormControl>  
+                <Flex>
+                  <Spacer />
+                  <Radio value='specific' spacing="4">
+                    <Heading fontSize="md" fontWeight="semibold">                        
+                      <Badge fontSize="md" colorScheme='green'>Específico</Badge>
+                    </Heading>
+                    <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
+                      El pack debe contener todos los productos
+                    </Text>
+                  </Radio>
+                  <Spacer />
+                  <Radio value='surprise' spacing="4">
+                    <Heading fontSize="md" fontWeight="semibold">                        
+                      <Badge fontSize="md" colorScheme='purple'>Sorpresa</Badge>
+                    </Heading>
+                    <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
+                      El pack puede contener algunos de los productos
+                    </Text>
+                  </Radio>
+                  <Spacer />
+                </Flex>
+                </RadioGroup>
+              </Box>
+              <Box>
+                <FormControl id="name" isRequired>
+                    <FormLabel>Stock disponible</FormLabel>
+                    <Input type="number" 
+                        placeholder="0"
+                        onInput={(e) => inputStock(e)}
+                        _placeholder={{ color: 'gray.500' }}
+                    />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="name" isRequired>
+                    <FormLabel>Consumir antes de</FormLabel>
+                    <Input type="datetime-local"
+                        placeholder="0"
+                        onInput={(e) => inputDateTime(e)}
+                        _placeholder={{ color: 'gray.500' }}
+                    />
+                </FormControl>
+              </Box>
+              <Box height='80px'></Box>
+            </SimpleGrid>
           </Stack>
         </Stack>
       </Box>
