ffd3eb82b5b4e86059892f2c1af16249c2a353cc
manudelca <manueldelcarril@gmail.com>
2023-07-17 01:00:26 -0300

    Added original price

--- components/shop/CreateMenuForm.tsx
+++ components/shop/CreateMenuForm.tsx
@@ -35,6 +35,7 @@ import { HiEye, HiEyeOff } from 'react-icons/hi'
     const [type, setType] = useState("specific")
     const [dateTime, setDateTime] = useState("")
     const [price, setPrice] = useState(0)
+    const [originalPrice, setOriginalPrice] = useState(0)
 
     const inputName = (event) => {
       maxLengthCheck(event, 50)
@@ -82,6 +83,11 @@ import { HiEye, HiEyeOff } from 'react-icons/hi'
     const inputPrice = (event) => {
       allowOnlyNumber(event)
       setPrice(Number(event.target.value))
+    }
+
+    const inputOriginalPrice = (event) => {
+      allowOnlyNumber(event)
+      setOriginalPrice(Number(event.target.value))
     }
 
     return <Box
@@ -201,6 +207,27 @@ import { HiEye, HiEyeOff } from 'react-icons/hi'
                         _placeholder={{ color: 'gray.500' }}
                     />
                     </InputGroup>
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="original_price" isRequired>
+                    <FormLabel>Precio original</FormLabel>
+                    <InputGroup>
+                    <InputLeftElement
+                      pointerEvents='none'
+                      color='gray.500'
+                      fontSize='1.2em'
+                      children='$'
+                    />
+                    <Input type="text"
+                        placeholder="0.00"
+                        onInput={(e) => inputOriginalPrice(e)}
+                        _placeholder={{ color: 'gray.500' }}
+                    />
+                    </InputGroup>
+                    <FormHelperText fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
+                      Contales a tus clientes cuál era el precio original de tu producto. Así saben cuánto ahorran a la par que cuidan el planeta :)
+                    </FormHelperText>
                 </FormControl>
               </Box>
               <Box height='80px'></Box>
